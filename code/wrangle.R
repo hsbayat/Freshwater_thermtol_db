4d7329872ef52b20cb0afa585cff88ad7db186dc
hsbayat <bayat@uni-landau.de>
2024-10-01 17:00:02 +0200

    data processing code

---
+++ code/wrangle.R
@@ -0,0 +1,50 @@
+
+
+# info --------------------------------------------------------------------
+
+# Final processing of freshwater thermal tolerance dataset
+
+# Helena S. Bayat
+
+# last edited: 01/10/2024
+
+# setup -------------------------------------------------------------------
+library(tidyverse)
+
+library(patchwork)
+library(ggridges)
+library(viridis)
+
+# load data ---------------------------------------------------------------
+
+tol <- read.csv('data/thermtol_tests.csv')
+tax <- read.csv('data/thermtol_taxonomy.csv')
+clim <- read.csv('data/thermtol_climate.csv')
+ref <- read.csv('data/thermtol_reference.csv')
+
+tt1 <- merge(tol, tax, by = "tax_id")
+tt2 <- merge(tt1, clim, by = "loc_id")
+dat <- merge(tt2, ref, by = "study_id")
+
+# wrangle -----------------------------------------------------------------
+
+# put units into common currency
+
+# for error measures - on thermal tolerance, body mass, and body length
+
+# time - for acclimation and test
+
+
+# write to file -----------------------------------------------------------
+
+# save the pieces
+data = 'data'
+write.csv(tax, file.path(data, 'thermtol_taxonomy.csv'), row.names = FALSE, na = "")
+write.csv(tol, file.path(data, 'thermtol_tests.csv'), row.names = FALSE, na = "")
+write.csv(clim, file.path(data, 'thermtol_climate.csv'), row.names = FALSE, na = "")
+write.csv(ref, file.path(data, 'thermtol_reference.csv'), row.names = FALSE, na = "")
+
+
+# end ---------------------------------------------------------------------
+
+rm(list = ls())
